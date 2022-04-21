5569d9a212572ffcb833083887fbd33307b5ee32
KraftOreo <976539567@qq.com>
2022-04-21 19:08:09 -0400

    test PSO

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <src/ParticleSwarmOptimization.h>
 #include <Eigen/Dense>
+#include <src/test.h>
+
 
 double fun(Eigen::Vector<double, 3> &x){
     return x.dot(x);
 }
 
 int main(int, char**) {
+    Balabala b;
+    b.GenRandom();
+    b.PrintMatrix();
     ParticalSwarmOptimization<double, 3> PSO(&fun);
     PSO.run();
     return 0;
